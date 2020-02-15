eb3942f8fe1d596577a259e04469b5ab7e9d92ad
shomendrap@gmail.com <shomendrap@gmail.com>
2020-02-15 20:02:18 +0530

    handle auth

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  mongoURI: 'mongodb+srv://storybooks-dev:shomendra@cluster0-wrcnu.mongodb.net/storybooks-dev?retryWrites=true&w=majority',
+  googleClientID: '290734536306-sdrsf37sksh37jj27nhkbjnrtvemt5tb.apps.googleusercontent.com',
+  googleClientSecret:'v66uaTp_uIKpvFDfegOW94aA'
+
+}
+
+
+
