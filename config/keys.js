92b43ed5fd8c69e4436c0342d5da3ea7df469d08
shomendrap@gmail.com <shomendrap@gmail.com>
2020-02-15 21:27:44 +0530

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  mongoURI: 'mongodb+srv://storybooks-dev:shomendra@cluster0-wrcnu.mongodb.net/storybooks-dev?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://storybooks-dev:shomendra@cluster0-wrcnu.mongodb.net/test?retryWrites=true&w=majority',
   googleClientID: '290734536306-sdrsf37sksh37jj27nhkbjnrtvemt5tb.apps.googleusercontent.com',
   googleClientSecret:'v66uaTp_uIKpvFDfegOW94aA'
 
